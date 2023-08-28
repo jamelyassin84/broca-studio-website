cb187877f5865e8d7576ced6e091f164d1e63a45
jamelyassin84@gmail.com <jamelyassin84@gmail.com>
2023-08-28 22:44:20 +0400

    FEAT: Designed Home Section 4

--- src/app/modules/guest/home/home-section4/home-section4.component.ts
+++ src/app/modules/guest/home/home-section4/home-section4.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { SharedModule } from 'app/shared/shared.module'
+import { CINEMATIC_FEATURES } from 'app/app-core/constants/cinematic-features'
 
 @Component({
-  selector: 'home-section4',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home-section4.component.html'
+	selector: 'home-section4',
+	standalone: true,
+	imports: [SharedModule],
+	templateUrl: './home-section4.component.html',
 })
 export class HomeSection4Component {
+	readonly CINEMATIC_FEATURES = CINEMATIC_FEATURES
 
+	trackByFn(index: number, item: any): any {
+		return item.id || index
+	}
 }
